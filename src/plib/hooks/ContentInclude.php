e20e00008d5129d3abb18e14456bcf79b685f481
Steven Mey <mail@steven-mey.de>
2021-11-11 20:07:01 +0100

    Suggestions from the Plesk Extensions Certification Team

--- src/plib/hooks/ContentInclude.php
+++ src/plib/hooks/ContentInclude.php
@@ -1,29 +1,34 @@
 <?php
 
-function f2bcheckIP($ip) {
-    $api_result = pm_ApiCli::call('ip_ban', ['--banned']);
-
-    preg_match("/$ip(.*)/", $api_result['stdout'], $result['Jails']);
-    $result['Jails'] = preg_replace("/$ip/", "", $result['Jails']);
-    $result['Jails'] = array_map('trim', $result['Jails']);
-    $result['Jails'] = array_unique($result['Jails']);
-
-    return $result;
-}
-
-function f2bunbanIP($ip) {
-    foreach (f2bcheckIP($ip)['Jails'] as $jail) {
-        $api_result .= pm_ApiCli::call('ip_ban', ['--unban', "$ip,$jail"]);
-    }
-    
-    return $api_result;
-}
-
 class Modules_fail2banalert_ContentInclude extends pm_Hook_ContentInclude {
+    
+    function f2bcheckIP($ip) {
+        $api_result = pm_ApiCli::call('ip_ban', ['--banned']);
+
+        preg_match("/$ip(.*)/", $api_result['stdout'], $result['Jails']);
+        $result['Jails'] = preg_replace("/$ip/", "", $result['Jails']);
+        $result['Jails'] = array_map('trim', $result['Jails']);
+        $result['Jails'] = array_unique($result['Jails']);
+
+        return $result;
+    }
+
+    function f2bunbanIP($ip) {
+        $api_result = "";
+
+        foreach (f2bcheckIP($ip)['Jails'] as $jail) {
+            $api_result .= pm_ApiCli::call('ip_ban', ['--unban', "$ip,$jail"]);
+        }
+        
+        return $api_result;
+    }
+
     public function getJsOnReadyContent()
     {
+        $msgtype = "";
+
         if (!isset($_GET['f2bunban'])) {
-            $check_result = f2bcheckIP($_SERVER['REMOTE_ADDR']);
+            $check_result = Modules_fail2banalert_ContentInclude::f2bcheckIP($_SERVER['REMOTE_ADDR']);
 
             if (count($check_result['Jails']) > 0) {
                 $msgtype = 'error';
@@ -34,7 +39,7 @@ class Modules_fail2banalert_ContentInclude extends pm_Hook_ContentInclude {
                 $msg .= '<a href="?f2bunban">'.pm_Locale::lmsg('link').'</a>';
             }
         } else {
-            f2bunbanIP($_SERVER['REMOTE_ADDR']);
+            Modules_fail2banalert_ContentInclude::f2bunbanIP($_SERVER['REMOTE_ADDR']);
             $msgtype = 'info';
             $msg = 'Your IP ('.$_SERVER['REMOTE_ADDR'].') was unbanned.';
         }
